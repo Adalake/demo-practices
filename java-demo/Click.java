adecef53ff23c4bad8538b0a019a1074846a258c
lake <gaomintv@sohu.com>
2020-01-06 16:22:16 +0800

    内部类&反射

--- java-demo/Click.java
+++ java-demo/Click.java
@@ -1,3 +1,7 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 // 输出：
 // form Person  
 // from Student
@@ -9,10 +13,44 @@ public class Click {
         Person ming = new Student("xiao ming");
         ming.run();
         ming.say("voice");
+
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("orange", 1);
+        map.put("apple", 2);
+        map.put("pear", 3);
+        for (String key : map.keySet()) {
+            System.out.println(key);
+        }
+
+        RearlStudent test = new RearlStudent();
+        test.update();
+
+        PetRestaurant dog = new Dog();
+        PetRestaurant cat = new Cat();
+        Test testNow = new Test();
+        testNow.buyx(dog); // 接口的作用
+        testNow.buyx(cat);
+        dog.buy(); // 如果没有Test这个类, 那么要调用buy()这个要写成这样
+        cat.buy(); // 如果没有Test这个类, 那么要调用buy()这个要写成这样
+        System.out.println("________");
+        new Cat(cat);
+
+        Map<PersonQ, Integer> mapq = new TreeMap<>(new Comparator<PersonQ>() {
+            public int compare(PersonQ p1, PersonQ p2) {
+                return p1.name.compareTo(p2.name);
+            }
+        });
+        mapq.put(new PersonQ("Tom"), 1);
+        mapq.put(new PersonQ("Bob"), 2);
+        mapq.put(new PersonQ("Lily"), 3);
+        for (PersonQ key : mapq.keySet()) {
+            System.out.println(key);
+        }
+        System.out.println(mapq.get(new PersonQ("Bob")));
     }
 }
 
-public class Person {
+class Person {
     public String name;
 
     public Person(String name) {
@@ -27,12 +65,12 @@ public class Person {
     }
 }
 
-public class Student extends Person {
+class Student extends Person {
     public String loud;
 
     public Student(String name) {
         super(name);
-        System.out.println("然后调用子类的构造方法，所以这条第二行输出：from Student");
+        System.out.println("然后调用子类的构造方法，所以这条第二行输出： from Student");
     }
 
     @Override
@@ -43,5 +81,64 @@ public class Student extends Person {
     @Override
     public void say(String loud) {
         System.out.println(loud + " student say");
+    }
+}
+
+interface students {
+    void update();
+}
+
+class RearlStudent implements students {
+    @Override
+    public void update() {
+        System.out.println("122");
+    }
+}
+
+interface PetRestaurant {
+    public void buy();
+}
+
+class Dog implements PetRestaurant {
+    @Override
+    public void buy() { // 这个类方法的修饰符是public
+        System.out.println("I am dog, I want to eat");
+    }
+}
+
+class Cat implements PetRestaurant {
+    public Cat(PetRestaurant pet) {
+        pet.buy();
+    }
+
+    public Cat() {
+    }
+
+    @Override
+    public void buy() {
+        System.out.println("I am cat, I want to eat");
+    }
+}
+
+// 下面这个类相当于一个接待顾客的类，即店小二，他接待所有实现了我这个宠物店接口的动物，传进来一个PetRestaurant
+// 类型的宠物，注意，这个PetRestaurant是接口
+class Test {
+    public void buyx(PetRestaurant pet) {
+        pet.buy();
+        System.out.println("from Test");
+    }
+}
+
+// 接口作为类的参数传入
+
+class PersonQ {
+    public String name;
+
+    PersonQ(String name) {
+        this.name = name;
+    }
+
+    public String toString() { // 如果没有这个,输出变成hash值?
+        return "{PersonQ: " + name + "}";
     }
 }
---
+++ java-demo/Reflection.java
@@ -0,0 +1,34 @@
+// JVM为每个加载的class创建了对应的Class实例,并在实例中保存了该class的所有信息
+// 通过Class实例获取class信息的方法称为反射
+
+public class Reflection {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException {
+        String s = "hello";
+        Class cls = s.getClass();
+        System.out.println(cls);
+        Class c1 = String.class;
+        System.out.println(c1);
+        Class c2 = Class.forName("java.lang.String");
+        System.out.println(c2);
+
+        Class StdClass = Student.class;
+        System.out.println(StdClass);
+        System.out.println(StdClass.getField("score"));
+        System.out.println(StdClass.getField("name"));
+        System.out.println(StdClass.getDeclaredField("grade"));
+        // System.out.println(StdClass.getField("grade")); // 获取不到
+        // System.out.println(StdClass.getDeclaredField("name")); // 获取不到
+    }
+}
+
+class Person {
+    public String name;
+}
+
+class Student extends Person {
+    public int score;
+    private int grade;
+}
+
+// getField: 获取一个类的public成员变量, 包括基类
+// getDeclaredField: 获取一个类的所有成员变量, 不包括基类
